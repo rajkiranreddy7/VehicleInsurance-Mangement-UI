8af322a0a600387c3688e21d6c0da5e85f0b226c
rajkiranreddy7 <rajkiranreddy7@gmail.com>
2024-05-10 23:47:47 +0530

    Added routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import Home from './functions/authentication/Home';
+import Header from './functions/admin/Header';
+import LoginForm from './functions/Authentication/LoginForm';
+import {store,persistor} from "./redux/store";
+import AdminSidebar from './functions/admin/AdminSidebar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <PersistGate loading={<h2>loading...</h2>} persistor={persistor}>
+        <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path="/login" element={<LoginForm/>} />
+          <Route path="/sidebar" element={<AdminSidebar/>}/>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   );
 }
 
